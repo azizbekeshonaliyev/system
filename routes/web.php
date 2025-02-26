9056c6441141e72922e704b32415cdd72720396d
azizbekeshonaliyev <1996azizbekeshonaliyev@gmail.com>
2025-02-26 18:04:53 +0500

    l

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('employees', function () {
+    return Inertia::render('Employees');
+})->middleware(['auth', 'verified'])->name('employees');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
